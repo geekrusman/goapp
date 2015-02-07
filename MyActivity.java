ca7cf21a27948e50cb3c2d454b738a292bfde234
bharath <kbreddy.rpbc@gmail.com>
2015-02-07 13:19:30 +0530

    updated the type of terrain

--- MyActivity.java
+++ MyActivity.java
@@ -2,6 +2,7 @@ package com.example.bharath.goapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -10,11 +11,36 @@ import android.view.MenuItem;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MyActivity extends Activity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MyActivity extends FragmentActivity
+        implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+
+
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
     }
 }
