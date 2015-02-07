60bd05a3ced7304c9ca10b76213fbcbc1c7a64a7
bharath <kbreddy.rpbc@gmail.com>
2015-02-08 01:42:02 +0530

    milestone1 : current location track

--- MyActivity.java
+++ MyActivity.java
@@ -13,6 +13,7 @@ import android.view.MenuItem;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -28,89 +29,80 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.common.api.*;
-public class MyActivity extends FragmentActivity
-        implements OnMapReadyCallback,GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,LocationListener {
 
-    private GoogleApiClient mGoogleApiClient;
-    private LatLngBounds AUSTRALIA = new LatLngBounds(
-            new LatLng(-44, 113), new LatLng(-10, 154));
-    LatLng MOUNTAIN_VIEW = new LatLng(37.4, -122.1);
-    private LocationRequest mLocationRequest;
-    LatLng curr;
+
+
+public class MyActivity extends FragmentActivity implements OnMapReadyCallback,GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener
+{
+    GoogleApiClient mGoogleApiClient;
     MapFragment mapFragment;
-
+    Location mLastLocation;
+    TextView tx ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-
-
-
-
-
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-
-         mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        createLocationRequest();
+        buildGoogleApiClient();
 
-
-
+        tx = (TextView)findViewById(R.id.textView);
 
     }
+
+    protected void onStart() {
+        super.onStart();
+         mGoogleApiClient.connect();
+
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener( this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
 
     @Override
     public void onMapReady(GoogleMap map) {
 
-
-
         map.addMarker(new MarkerOptions()
-                .position(new LatLng(0,0))
+                .position(new LatLng(0, 0))
                 .title("Marker"));
         map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-
-        //LatLngBounds AUSTRALIA = new LatLngBounds(new LatLng(-44, 113), new LatLng(-10, 154));
-
-        //map.moveCamera(CameraUpdateFactory.newLatLngBounds(AUSTRALIA, 0));
-
-
-       // map.moveCamera(CameraUpdateFactory.newLatLngZoom(AUSTRALIA.getCenter(), 10));
-
-
-        map.animateCamera(CameraUpdateFactory.zoomIn());
-
-        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
-
-
-
-
-        CameraPosition cameraPosition = new CameraPosition.Builder()
-                .target(MOUNTAIN_VIEW)      // Sets the center of the map to Mountain View
-                .zoom(17)                   // Sets the zoom
-                .bearing(90)                // Sets the orientation of the camera to east
-                .tilt(30)                   // Sets the tilt of the camera to 30 degrees
-                .build();                   // Creates a CameraPosition from the builder
-        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-
-
-
-
         map.setMyLocationEnabled(true);
+
+
     }
 
     @Override
     public void onConnected(Bundle bundle) {
 
-        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
                 mGoogleApiClient);
 
-        startLocationUpdates();
+        tx.setText(String.valueOf(mLastLocation.getLatitude()) + "  " + String.valueOf(mLastLocation.getLongitude()));
 
 
+       GoogleMap map = mapFragment.getMap();
+
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(mLastLocation.getLatitude(),mLastLocation.getLongitude()))      // Sets the center of the map to Mountain View
+                .zoom(17)                   // Sets the zoom
+                .bearing(90)                // Sets the orientation of the camera to east
+                .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
     }
 
@@ -123,49 +115,5 @@ public class MyActivity extends FragmentActivity
     public void onConnectionFailed(ConnectionResult connectionResult) {
 
     }
-
-    @Override
-    public void onLocationChanged(Location location) {
-
-            double lat = location.getLatitude();
-            double longi = location.getLongitude();
-
-
-
-
-
-
-
-
-    }
-
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-
-    }
-
-    protected void createLocationRequest() {
-        mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(10000);
-        mLocationRequest.setFastestInterval(5000);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-    }
-
-    protected void startLocationUpdates() {
-        LocationServices.FusedLocationApi.requestLocationUpdates(
-                mGoogleApiClient, mLocationRequest, (com.google.android.gms.location.LocationListener) this);
-
-
-    }
 }
+
