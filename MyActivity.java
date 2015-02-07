7954b0f791e24692bda7e6032e2c8e67c9b79340
Bharath <kbreddy.rpbc@gmail.com>
2015-02-07 09:57:52 +0530

    Initial commit

---
+++ MyActivity.java
@@ -0,0 +1,20 @@
+package com.example.bharath.goapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+}
